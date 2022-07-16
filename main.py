a4af8d282e5e9b9466fefaf02961f79970e660b2
middle <gamloft7@gmail.com>
2022-07-16 10:08:08 +0300

    Универсинализировал для почт муктр

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import pandas as pd
 
 
 def check_mail(mail, psw):
+    muctr_465 = False
     if "gmail.com" in mail:
         server = smtplib.SMTP('smtp.gmail.com:587')
         server.starttls()
@@ -16,12 +17,18 @@ def check_mail(mail, psw):
     elif "mail.ru" in mail:
         server = smtplib.SMTP_SSL('smtp.mail.ru:465')
     elif "muctr.ru" in mail:
-        server = smtplib.SMTP('post.muctr.ru:587')
-        server.starttls()
+        server = smtplib.SMTP_SSL('smtp.muctr.ru:465')
+        try:
+            server.login(mail, psw)
+            muctr_465 =True
+        except:
+            server = smtplib.SMTP('post.muctr.ru:587')
+            server.starttls()
     else:
         return False
     try:
-        server.login(mail, psw)
+        if not muctr_465:
+            server.login(mail, psw)
     except:
         return False
     return True
@@ -30,7 +37,8 @@ def check_mail(mail, psw):
 def send_mail(in_file, mail, psw, header, html_text):
     people = pd.read_excel('sending.xlsx')
     count = 0
-    yandex =False
+    yandex = False
+    muctr_465 = False
     if "gmail.com" in mail:
         server = smtplib.SMTP('smtp.gmail.com:587')
         server.starttls()
@@ -40,12 +48,18 @@ def send_mail(in_file, mail, psw, header, html_text):
     elif "mail.ru" in mail:
         server = smtplib.SMTP_SSL('smtp.mail.ru:465')
     elif "muctr.ru" in mail:
-        server = smtplib.SMTP('post.muctr.ru:587')
-        server.starttls()
+        server = smtplib.SMTP_SSL('smtp.muctr.ru:465')
+        try:
+            server.login(mail, psw)
+            muctr_465 = True
+        except:
+            server = smtplib.SMTP('post.muctr.ru:587')
+            server.starttls()
     else:
         print("Неопознанный домен")
         exit(0)
-    server.login(mail, psw)
+    if not muctr_465:
+        server.login(mail, psw)
     for human in people.values:
         print(human[0], human[1])
         msg = MIMEMultipart()
@@ -56,8 +70,11 @@ def send_mail(in_file, mail, psw, header, html_text):
         for key in in_file.keys():
             dict_const[key] = human[in_file[key]]
         msg.attach(MIMEText(html_text.format(**dict_const), 'html', 'utf-8'))
-        server.sendmail(mail, human[0], msg.as_string())
-        count +=1
+        try:
+            server.sendmail(mail, human[0], msg.as_string())
+        except:
+            print("Не отправлено")
+        count += 1
         print('{}/{}'.format(count, len(people.values)))
         time.sleep(10)
     server.quit()
